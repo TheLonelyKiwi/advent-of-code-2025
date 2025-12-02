2e9371c0d384cb74ec789e4df378104113058d23
TheLonelyKiwi <gamingkiwi22@gmail.com>
2025-12-02 10:57:05 +0100

    day 2 yay

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ using advent_of_code_2025.solutions;
 string[] input = File.ReadAllLines("../../../input.txt");
 
 Day1 day1 = new Day1(input);
+Day2 day2 = new Day2(input);
 
-day1.Solve2();
+day2.Solve2();
---
+++ solutions/Day2.cs
@@ -0,0 +1,65 @@
+﻿namespace advent_of_code_2025.solutions;
+
+public class Day2(string[] lines)
+{
+    public void Solve1()
+    {
+        Int64 total = 0;
+
+        foreach (string pair in lines[0].Split(','))
+        {
+            var range = pair.Split('-');
+            long first = long.Parse(range[0]);
+            long last = long.Parse(range[1]);
+
+            for (long i = first; i <= last; i++)
+            {
+                var numString = i.ToString();
+                var length  = numString.Length;
+
+                if (length >= 2)
+                {
+                    int midPoint =  length / 2;
+                    if (numString.Substring(0, midPoint) == numString.Substring(midPoint))
+                    {
+                        Console.WriteLine("Value:" + numString);
+                        total += long.Parse(numString);
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine(total);
+    }
+    
+    public void Solve2()
+    {
+        Int64 total = 0;
+
+        foreach (string pair in lines[0].Split(','))
+        {
+            var range = pair.Split('-');
+            long first = long.Parse(range[0]);
+            long last = long.Parse(range[1]);
+
+            for (long i = first; i <= last; i++)
+            {
+                var numString = i.ToString();
+
+                for (int j = 1; j <= numString.Length; j++)
+                {
+                    string chunk = numString.Substring(0,j);
+                    string block = numString.Replace(chunk, "s");
+
+                    if (block == "s")
+                    {
+                        total += i;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine(total);
+    }
+}
